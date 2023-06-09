689a9438197d04390e5267eea7936e80a98645f0
jpmattin <56087241+jpmattin@users.noreply.github.com>
2023-06-08 19:06:30 -0700

    Update ScoreCalculator.py

--- ScoreCalculator.py
+++ ScoreCalculator.py
@@ -1,11 +1,15 @@
+import sys
 '''
-Calculates the score of the output sequences of a MAFFT alignment.
-A value of 1 is added to the score if the character at the current
-index of the 3 sequences are equal. 
-
-returns score
+ScoreCalculor.py is a python script that takes in a text file
+containing the 3 sequence alignment with no headers and returns
+the score and length of the alignment.
 '''
 
+'''
+Calculates the score of the alignment of three sequences.
+A value of 1 is added to the score if the character at the
+index of the 3 sequences is the same. 
+'''
 def CalculateScore(s1, s2, s3):
     if len(s1) != len(s2) and len(s3):
         raise ValueError("All sequences must have the same length")
@@ -17,4 +21,21 @@ def CalculateScore(s1, s2, s3):
 
     return score
 
+# Get the FASTA file path from the command-line argument
+file_path = sys.argv[1]
+
+# Check if the correct number of command-line arguments is provided
+if len(sys.argv) != 2:
+    print("python ScoreCalculator.py [alignments file]")
+    sys.exit(1)
+
+sequences = []
+with open(file_path, 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        sequences.append(line.strip())
+
+# Prints the alignment score and alignment length of an alignment
+print('Alignment score: ' + str(CalculateScore(sequences[0], sequences[1], sequences[2])))
+print('Aligntment length: ' + str(len(sequences[0])))
 
